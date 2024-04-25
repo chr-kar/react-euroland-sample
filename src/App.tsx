9d2971226ae25bf99acae90df8f2b5369e3a48bf
CKS <cks@novozymes.com>
2024-04-25 11:20:55 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        data-euroland-tool
+        data-tool-name="sharegraph3"
+        data-tool-company-code="s-nda"
+        data-tool-url-base="https://gamma.euroland.com/tools/"
+      />
     </div>
   );
 }
